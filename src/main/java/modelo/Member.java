bdde2c603aee1c6ca85af731e684bc0edeafbb7e
@RedondaAnalia <a.redonda89@gmail.com>
2018-03-16 15:55:15 -0300

    Prueba Travis

---
+++ src/main/java/modelo/Member.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Member {
+	private double credit;
+	
+	public Member(){
+		credit=40.0;
+	}
+	
+	public double addCredit(double toCredit){
+		credit+=toCredit;
+		return credit;
+	}
+	
+	public double getCredit(){
+		return credit;
+	}
+
+}
---
+++ src/test/java/tests/MemberTestCase.java
@@ -0,0 +1,26 @@
+package tests;
+
+import modelo.Member;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MemberTestCase {
+	
+	private Member member;
+
+	@Before
+	public void setUp() throws Exception {
+		member= new Member();
+	}
+	
+	@Test
+	public void CreditTest(){
+		member.addCredit(40.0);
+		assertEquals(80.0, member.getCredit(), 0);
+	}
+
+}
