814f09ce414e25f8de99a8f80a0312b686381936
@RedondaAnalia <a.redonda89@gmail.com>
2018-03-22 21:24:11 -0300

    Se establecen limites de tiempo de alquiler. SIN TESTEAR.

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -1,26 +1,46 @@
 package model;
 
+import java.time.*;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+
+import org.omg.DynamicAny.DynAnySeqHelper;
 
 public class Post {
 
 	Vehicle vehicle;
-	User postUser;
-	User rentUser;
+	User ownerUser;
+	User tenantUser;
 	Coord pickUpCoord;
 	ArrayList<Coord> returnCoords;
+	LocalDateTime available;
+	LocalDateTime maximumReturnTime;
 	//double costoPorDia;
 	//double costoPorHora;
 	//int phone
 	//hora y dias disponibles para alquilar
 	
 	
+	
+//************************************************************************************************************************
+//  ESTO NO ESTA TESTEADO, NI SIQUIERA SE SI FUNCIONA ASI. SOLO ESCRIBI EL CODIGO QUE ME PARECIO QUE IBA A FUNCIONARR!!!	
+//************************************************************************************************************************  
 	public Post(Vehicle vehicle, User user, Coord pickUpCoord, ArrayList<Coord> returnCoords){
+		
+		long hours= available.until(maximumReturnTime,ChronoUnit.HOURS);
+		long days= available.until(maximumReturnTime, ChronoUnit.DAYS);
+		
+		if( hours<1 || days>=5 ){
+			throw new RuntimeException("El tiempo minimo de alquiler es 1 hora y el maximo 5 dias.");
+		}
+		
 		this.vehicle=vehicle;
-		this.postUser=user;
+		this.ownerUser=user;
 		this.pickUpCoord=pickUpCoord;
 		this.returnCoords=returnCoords;
-		this.rentUser= null;  
+		this.tenantUser= null;  
 	}
 	
 	
