2a3906d13c401d509c49f7545f3fb39351b7cf01
marivgil <gil.maricruz@gmail.com>
2018-04-22 20:10:44 -0300

    Dejó de explotar el jetty, vamo!!!

--- src/main/java/service/UserRest.java
+++ src/main/java/service/UserRest.java
@@ -1,8 +1,7 @@
 package service;
 
 import model.User;
-import persistence.repositories.UserRepository;
-
+import persistence.services.UserService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,13 +11,21 @@ import java.util.List;
 @Path("/services")
 public class UserRest{
 
-    private UserRepository userRepository;
+    private UserService userService;
+
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+    public UserService getUserService(){
+        return this.userService;
+    }
 
     @GET
     @Path("/byCuil/{cuil}")
     @Produces("application/json")
     public List<User> findUserByCUIL(@PathParam("cuil") final String cuil){
-        return this.userRepository.filterUser(cuil);
+        return this.userService.filterUser(cuil);
     }
 
 }
