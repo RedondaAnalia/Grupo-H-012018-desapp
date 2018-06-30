5ce09f8b9b3e8167966b09c180911692390bc5e4
marivgil <gil.maricruz@gmail.com>
2018-06-30 19:39:51 -0300

    primer versión de envío de mails

--- src/main/java/model/Mail.java
+++ src/main/java/model/Mail.java
@@ -1,23 +1,63 @@
 package model;
 
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
 public class Mail {
-/*
-    private MailSender mailSender;
 
-    public void setMailSender(MailSender mailSender) {
-        this.mailSender = mailSender;
+    private static String USER_NAME = "carpnd89";
+    private static String PASSWORD = "Carpnd89";
+    private static String RECIPIENT = "gil.maricruz@gmail.com";
+
+    public static void sendMail() {
+        String from = USER_NAME;
+        String pass = PASSWORD;
+        String[] to = { RECIPIENT };
+        String subject = "Java send mail example";
+        String body = "Welcome to JavaMail!";
+
+        sendFromGMail(from, pass, to, subject, body);
     }
 
-    public void sendMail(String from, String to, String subject, String msg) {
-        //creating message
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(msg);
-        //sending message
-        mailSender.send(message);
-    }
+    private static void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
 
-    */
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
 }
