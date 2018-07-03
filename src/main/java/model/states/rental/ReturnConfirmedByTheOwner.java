6000baf16c73a7de4f58c167712e6a3c47f3b4c5
marivgil <gil.maricruz@gmail.com>
2018-07-02 23:29:39 -0300

    fix: problema de debito duplicado del dinero de las cuentas

--- src/main/java/model/states/rental/ReturnConfirmedByTheOwner.java
+++ src/main/java/model/states/rental/ReturnConfirmedByTheOwner.java
@@ -7,7 +7,6 @@ import model.Rental;
 import model.exceptions.InvalidStatusChangeException;
 import model.interfaces.IRentalState;
 
-import java.time.LocalDateTime;
 
 public class ReturnConfirmedByTheOwner extends IRentalState {
 
@@ -25,9 +24,6 @@ public class ReturnConfirmedByTheOwner extends IRentalState {
 
     public void tenantUserConfirmated(Rental rental, Integer score, String comment) {
         rental.getOwnerUser().processScore(score);
-        LocalDateTime endRentalTime = LocalDateTime.now();
-        AccountManager.processPayment(rental.rentCost(endRentalTime),
-                rental.getTenantUser(), rental.getOwnerUser());
         rental.setTenantComment(comment);
         rental.setState(new FinalizedRentalST());
 
